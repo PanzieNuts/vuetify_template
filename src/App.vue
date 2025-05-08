a434db3923e5c6a7c6e6ad10d814f03f541f612f
PanzieNuts <trishacarvajal55@gmail.com>
2025-05-08 15:58:59 +0800

    remove scroll

--- src/App.vue
+++ src/App.vue
@@ -5,5 +5,18 @@
 </template>
 
 <script setup>
-  import { ref } from 'vue'
+import { ref } from 'vue'
 </script>
+
+<style>
+/* Hide scrollbar globally */
+html, body {
+  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none;  /* IE 10+ */
+}
+
+html::-webkit-scrollbar,
+body::-webkit-scrollbar {
+  display: none; /* Chrome, Safari, Edge */
+}
+</style>
