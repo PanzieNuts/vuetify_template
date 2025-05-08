3e96c2027e9f32ea4807fdd40ea065d54b0a97b5
PanzieNuts <trishacarvajal55@gmail.com>
2025-05-08 15:49:35 +0800

    vuetify

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+        <h1>Home</h1>
+        <p>Welcome to the home page!</p>
+    </v-container>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import home from '../components/Home.vue'
+
+const routes = [
+  {
+    path: '/home',
+    name: 'Home',
+    component: home
+  },
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes,
+})
+
+export default router
